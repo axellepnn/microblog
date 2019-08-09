cfefa0d4d3df3845f18a6fdcc8ffdeb16279cf68
jetilton <jfftilton@gmail.com>
2019-08-08 19:31:55 -0700

    chapter 1

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,11 @@
 from app import app
+
+
 @app.route('/')
 @app. route('/index')
 def index():
     return "Hello World!"
+
+@app. route('/french')
+def french():
+    return "bonjour tout le monde!"
